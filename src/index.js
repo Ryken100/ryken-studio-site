a1a68ee8f8bb6bf3ea58904f547124d6b19f3703
Arlodotexe <chuck.fecht@outlook.com>
2018-08-06 15:02:17 -0500

    First working version of nav logo replacement on scroll

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './style/main.scss';
+
+function isNearTop() {
+    if (!((document.documentElement && document.documentElement.scrollTop) ||
+        (document.scrollingElement && document.scrollingElement.scrollTop) ||
+        document.body.scrollTop > 100)) return true;
+    else return false;
+}
+
+function isNearBottom() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) return true;
+    else return false;
+}
+
+function isScrolledIntoView(el, relElem) {
+    if (el && relElem) {
+        var rect = el.getBoundingClientRect();
+        var elemTop = rect.top + relElem.getBoundingClientRect().height;
+        var elemBottom = rect.bottom - relElem.getBoundingClientRect().height;
+        return elemTop < window.innerHeight && elemBottom >= 0;
+    } else {
+        throw new Error('Params for isScrolledIntoView() are not defined properly ')
+    }
+}
+
+document.addEventListener('scroll', () => {
+    if (!isScrolledIntoView(document.getElementById('logo'), document.getElementsByTagName('nav')[0])) {
+        console.log('gone');
+        document.getElementById('main-link-icon').classList.add('slidedown');
+        document.getElementById('main-link-icon').classList.remove('slideup');
+
+        document.getElementById('main-link-text').classList.add('slideup');
+        document.getElementById('main-link-text').classList.remove('slidedown');
+
+    } else {
+        document.getElementById('main-link-text').classList.add('slidedown');
+        document.getElementById('main-link-text').classList.remove('slideup');
+
+        document.getElementById('main-link-icon').classList.remove('slidedown');
+        document.getElementById('main-link-icon').classList.add('slideup');
+
+    }
+});
